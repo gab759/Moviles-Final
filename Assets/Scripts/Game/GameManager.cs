7e8b75bcd35ab0fcb90da8dd1deec5881a7dd7c9
Tecsup <miguel.vega@tecsup.edu.pe>
2025-06-17 15:43:40 -0500

    menu de pausa y cosas

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    public int vidaAdicionalParaJefe = 0;
+    public int puntaje = 0;
+    public bool juegoEnPausa { get; private set; } = false;
 
     private void Awake()
     {
+        Time.timeScale = 1f;
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
@@ -16,14 +21,23 @@ public class GameManager : MonoBehaviour
             
         }
     }
-
-
-    
-    public int vidaAdicionalParaJefe = 0;
-    public int puntaje = 0;
-
     public void AnadirPuntos(int puntosAGanar)
     {
         puntaje += puntosAGanar;
+    }
+    public void PausarJuego()
+    {
+        juegoEnPausa = true;
+        Time.timeScale = 0f;
+    }
+    public void ReanudarJuego()
+    {
+        juegoEnPausa = false;
+        Time.timeScale = 1f; 
+    }
+    public void ReiniciarJuego()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("game2");
     }
 }
--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -5,12 +5,35 @@ public class UIManager : MonoBehaviour
 {
     [Header("Elementos de UI")]
     [SerializeField] private TextMeshProUGUI textoPuntaje;
-
+    [SerializeField] private GameObject panelPausa;
     void Update()
     {
         if (textoPuntaje != null)
         {
             textoPuntaje.text = "Puntos: " + GameManager.Instance.puntaje;
+        }
+    }
+    public void OnClick_Pausar()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.PausarJuego();
+            panelPausa.SetActive(true); // Mostramos el panel de pausa
+        }
+    }
+    public void OnClick_Reanudar()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ReanudarJuego();
+            panelPausa.SetActive(false); // Ocultamos el panel de pausa
+        }
+    }
+    public void OnClick_Reiniciar()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.ReiniciarJuego();
         }
     }
 }
